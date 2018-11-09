3397a174d2038be13bfc7ea61e8c89f26fe65218
slavikzdebskyy <zdebskyy.slavik@gmail.com>
2018-11-09 02:08:55 +0200

    Implemented two functions: rotateMatrix and dragonCurve whith tests

--- src/dragon-curve/index.js
+++ src/dragon-curve/index.js
@@ -1,3 +1,18 @@
 export const dragonCurve = n => {
-  // logic...
+
+  const reverseString = string => {     
+    const result = string.split('');
+    for (let i = 0; i < result.length; i++) {
+      result[i] = (result[i] === '1') ? '0' : '1';
+    }    
+    return result.reverse().join('');
+  };  
+  let dragonRow = '1';
+
+  n -= 1;
+  while (n > 0){
+    dragonRow += '1' + reverseString(dragonRow);
+    n -= 1;
+  }
+  return dragonRow;
 };
--- src/rotate-matrix/index.js
+++ src/rotate-matrix/index.js
@@ -1,4 +1,14 @@
-export const rotateMatrix = arr => {
-  // logic...
-};
+ export const rotateMatrix = arr => {
+  const resultArray = [];
+  const len = arr.length;
+  let rowArray = [];
 
+  for(let col = 0; col < len; col++){
+    for(let row = 0; row < len; row++){
+      rowArray.push(arr[row][col]);      
+    }
+    resultArray.push(rowArray);
+    rowArray = [];    
+  }  
+  return resultArray;
+};
