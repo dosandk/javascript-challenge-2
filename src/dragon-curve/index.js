7bf43787ab5f44f5954b53d07bf5da0fe022e21c
Taras <tsamoilenko97@gmail.com>
2018-11-10 14:50:21 +0200

    dragon curve test passed

--- src/dragon-curve/index.js
+++ src/dragon-curve/index.js
@@ -1,3 +1,14 @@
-export const dragonCurve = n => {
-  // logic...
+const dragonCurve = n => {
+  const arr = [1];
+
+  for (let i = 1; i < n; i++) {
+    const reverseArr = arr.map(item => {
+      return item === 1 ? 0 : 1;
+    });
+    reverseArr.reverse();
+    arr.push(1, ...reverseArr);
+  }
+  return arr.join('');
 };
+
+module.exports = dragonCurve;
