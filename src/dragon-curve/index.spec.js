b2a63e9e77d326c045f91b8b8f539f0fe699d253
denys <den.hodovanets@gmail.com>
2018-11-12 17:24:40 +0200

    first-comm

--- src/dragon-curve/index.spec.js
+++ src/dragon-curve/index.spec.js
@@ -1,5 +1,19 @@
-import {dragonCurve} from './index'
+import {dragonCurve} from './index';
 
 describe('dragonCurve::', () => {
-  // need to implement
+  it('Function returns dragon', () => {
+    expect(dragonCurve(1)).toEqual('1');
+  });
+
+  it('Function returns dragon', () => {
+    expect(dragonCurve(2)).toEqual('110');
+  });
+
+  it('Function returns dragon', () => {
+    expect(dragonCurve(3)).toEqual('1101100');
+  });
+
+  it('Function returns dragon', () => {
+    expect(dragonCurve(5)).toEqual('1101100111001001110110001100100');
+  });
 });
--- src/rotate-matrix/index.js
+++ src/rotate-matrix/index.js
@@ -1,4 +1,12 @@
 export const rotateMatrix = arr => {
-  // logic...
+  const newArr = [];
+  for (let j = 0; j < arr.length; j++) {
+    const arrItems = [];
+    arr.forEach(item => {
+      arrItems.push(item[j]);
+    });
+    newArr.push(arrItems);
+  }
+  return newArr;
 };
 
