5eec13820de9d552be6e89d214aef8197464b01a
nazzarkevich <nazzik.3@gmail.com>
2018-11-13 01:40:08 +0200

    added rotate-matrix function

--- src/rotate-matrix/index.js
+++ src/rotate-matrix/index.js
@@ -1,4 +1,30 @@
 export const rotateMatrix = arr => {
-  // logic...
+  const matrix = [...arr];
+
+  const emptyMatrix = createMatrix(matrix.length);
+  const reversedMatrix = reverseMatrix(matrix, emptyMatrix);
+
+  return reversedMatrix;
 };
 
+export const createMatrix = length => {
+  const matrix = [];
+
+  for (let i = 0; i < length; i++) {
+    if (matrix.length < length) {
+      matrix[i] = new Array(length);
+    }
+  }
+
+  return matrix;
+};
+
+export const reverseMatrix = (matrix, emptyMatrix) => {
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix.length; j++) {
+      emptyMatrix[i][j] = matrix[j][i];
+    }
+  }
+
+  return emptyMatrix;
+};
--- src/rotate-matrix/index.spec.js
+++ src/rotate-matrix/index.spec.js
@@ -1,5 +1,30 @@
-import {rotateMatrix} from './index'
+import { createMatrix, reverseMatrix, rotateMatrix } from './index';
 
 describe('rotateMatrix::', () => {
-  // need to implement
+  it('should return correct array length', () => {
+    // Given
+    const length = 4;
+
+    // Then
+    expect(createMatrix(length).length).toEqual(length);
+  });
+
+  it('should return reverse matrix', () => {
+    // Given
+    const emptyMatrix = [[], []];
+    const matrix = [['1', '2'], ['3', '4']];
+    const expected = [['1', '3'], ['2', '4']];
+
+    // Then
+    expect(reverseMatrix(matrix, emptyMatrix)).toEqual(expected);
+  });
+
+  it('should rotate array', () => {
+    // Given
+    const array = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']];
+    const expected = [['1', '4', '7'], ['2', '5', '8'], ['3', '6', '9']];
+
+    // Then
+    expect(rotateMatrix(array)).toEqual(expected);
+  });
 });
