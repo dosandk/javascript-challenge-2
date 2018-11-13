3f6c900dcfdb3d5c38be2ee07e66f67af0c24ff7
Dm Mh <dmmhs888@gmail.com>
2018-11-13 22:14:28 +0200

    Add two functions with tests

--- src/dragon-curve/index.js
+++ src/dragon-curve/index.js
@@ -1,3 +1,30 @@
 export const dragonCurve = n => {
-  // logic...
+  let res = '1';
+  if (n === 1) {
+    return n;
+  } else {
+    for (let i = 1; i < n; i++) {
+      let a;
+      let b;
+      let c;
+      let d;
+      let e;
+      if (res.length === 1 && (n ^ 0) === n) {
+        a = res;
+        b = '1';
+        c = res[0] === '1' ? '0' : '1';
+        res = a + b + c;
+      } else if (res.length > 1 && res.length % 2 !== 0 && (n ^ 0) === n) {
+        a = res;
+        b = '1';
+        c =  res.slice(0, res.length / 2);
+        d =  res[Math.floor(res.length / 2)] === '1' ? '0' : '1';
+        e =  res.slice(Math.round(res.length / 2), res.length);
+        res = a + b + (c + d + e);
+      } else {
+        res = alert('Invalid function attribute!');
+      }
+    }
+    return res;
+  }
 };
--- src/rotate-matrix/index.js
+++ src/rotate-matrix/index.js
@@ -1,4 +1,11 @@
 export const rotateMatrix = arr => {
-  // logic...
+  const res_matrix = [];
+  for (let i = 0; i < arr.length; i++) {
+    res_matrix.push([]);
+    for (let j = 0; j < arr.length; j++) {
+      res_matrix[i].push(arr[j][i]);
+    }
+  }
+  return res_matrix;
 };
 
