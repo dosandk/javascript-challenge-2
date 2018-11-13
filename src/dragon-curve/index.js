b63b3969e526e38394b9c90b30396e9c5a63476d
IliaTaboo <iliataboo@gmail.com>
2018-11-13 22:47:28 +0200

    Add implementation & tests for dragon curve function

--- src/dragon-curve/index.js
+++ src/dragon-curve/index.js
@@ -1,4 +1,15 @@
-/* eslint-disable-next-line */
 export const dragonCurve = n => {
-  // logic...
+  let result = '1';
+
+  const flip = str =>
+    [...str]
+      .map(val => parseInt(val, 10) === 1 ? 0 : 1)
+      .reverse()
+      .join('');
+
+  for (let i = 1; i < n; i++) {
+    result += '1' + flip(result);
+  }
+
+  return parseInt(result, 10);
 };
--- src/dragon-curve/index.spec.js
+++ src/dragon-curve/index.spec.js
@@ -1,6 +1,19 @@
-/* eslint-disable-next-line */
-import {dragonCurve} from './index'
+import {dragonCurve} from './index';
 
 describe('dragonCurve::', () => {
-  // need to implement
+  it('Return curve of 1 bend', () => {
+    expect(dragonCurve(1)).toEqual(1);
+  });
+  it('Return curve of 2 bends', () => {
+    expect(dragonCurve(2)).toEqual(110);
+  });
+  it('Return curve of 3 bends', () => {
+    expect(dragonCurve(3)).toEqual(1101100);
+  });
+  it('Return curve of 4 bends', () => {
+    expect(dragonCurve(4)).toEqual(110110011100100);
+  });
+  it('Return curve of 5 bends', () => {
+    expect(dragonCurve(5)).toEqual(1101100111001001110110001100100);
+  });
 });
