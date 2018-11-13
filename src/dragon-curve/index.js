cca49aa088111841c2313a87022a873511f639a1
harrion <andriy.gaidar99@gmail.com>
2018-11-13 16:35:11 +0200

    Eslint fixes

--- src/dragon-curve/index.js
+++ src/dragon-curve/index.js
@@ -1,3 +1,8 @@
 export const dragonCurve = n => {
-  // logic...
+  if (n === 1) return 1;
+  const part = String(dragonCurve(n - 1));
+  return Number(part + 1 + part.split('')
+    .reverse()
+    .map(i => +!+i)
+    .join(''));
 };
