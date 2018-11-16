f57f282d66b57f589f61dee960543c7487ee8e0a
RSmetanyuk <rsmetanyuk@gmail.com>
2018-11-16 02:44:23 +0200

    implemented rotateMatrix

--- src/rotate-matrix/index.js
+++ src/rotate-matrix/index.js
@@ -1,4 +1,21 @@
 export const rotateMatrix = arr => {
-  return arr;  
+  const rusalt = [];
+  const arrLength = arr.length;
+  //const arrflat = arr.flat(); так коротше але тести чомусь не проходять. На jsfiddle працює.
+  const arrflat = arr.reduce((acc, val) => acc.concat(val), []);
+  const arrflatObj = arrflat.map((el,i) => {
+    return {
+      element: el, 
+      weight: i % arrLength
+    };
+  });
+  const arrflatObjSorted = arrflatObj.sort((a, b) => {
+    return a.weight - b.weight;
+  });
+  const resultArrFlat = arrflatObjSorted.map(a => a.element);
+  while (resultArrFlat.length > 0) {
+    rusalt.push(resultArrFlat.splice(0, arrLength));
+  }
+  return  rusalt;
 };
 
