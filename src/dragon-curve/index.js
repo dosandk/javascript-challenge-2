da30089bcb5d37245f644d3af9c19f9faaa8df81
nazzarkevich <nazzik.3@gmail.com>
2018-11-19 14:24:01 +0200

    added dragon-curve function with tests

--- src/dragon-curve/index.js
+++ src/dragon-curve/index.js
@@ -1,3 +1,23 @@
-export const dragonCurve = () => {
-  // logic...
+export const dragonCurve = n => {
+  let curve = '1';
+
+  for (let i = 2; i <= n; i++) {
+    let acc = '1';
+    let prev = '1';
+
+    for (let j = 0; j < curve.length; j++) {
+      acc += curve[j];
+
+      if (prev === '0') {
+        acc += '1';
+        prev = '1';
+      } else {
+        acc += '0';
+        prev = '0';
+      }
+    }
+    curve = acc;
+  }
+
+  return curve;
 };
--- src/dragon-curve/index.spec.js
+++ src/dragon-curve/index.spec.js
@@ -1,7 +1,20 @@
+import { dragonCurve } from './index';
 describe('dragonCurve::', () => {
-  it('one', () => {
-    const one = 1;
+  it('should output 110', () => {
+    // Given
+    const n = '2';
+    const expected = '110';
 
-    expect(one).toBe(1);
+    // Then
+    expect(dragonCurve(n)).toBe(expected);
+  });
+
+  it('should output 110110011100100', () => {
+    // Given
+    const n = '4';
+    const expected = '110110011100100';
+
+    // Then
+    expect(dragonCurve(n)).toBe(expected);
   });
 });
